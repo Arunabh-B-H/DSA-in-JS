f29c632d1651c7b5d5b70602201fe57949cda99a
Arunabh BH <arunabh2023@gmail.com>
2026-04-04 05:45:26 +0530

    Done with this

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 //Sum of two integers
 
-// let a = 5;
-// let b = 10;
-// let sum = a + b;
-// console.log(sum);
+// // let a = 5;
+// // let b = 10;
+// // let sum = a + b;
+// // console.log(sum);
 
-//Concatenation
+// Concatenation
 
-let a = "Hello, ";
-let b = 2;
-console.log(typeof (b + a));
+// let a = "Hello, ";
+// let b = 2;
+// console.log(typeof (b + a));
 
 // Here the the sum of a string and number is a string
+
+let a = 2;
+let b = 3;
+console.log("Sum of a and b" + a + b);
+//Here the operand sum of a and b is in first priority so it takes the string and make the rest of the integers as string
+// Sum of a and b- which is a string +a =  Sum of a and b 2 + 5 = Sum of a and b 23
+//How to fix it?
+console.log("Sum of a and b is " + (a + b));
+//here the inside the bracket it gets the priority
+//if
+console.log(a + b  +  " is sum of a and b");
+//here the 2 + 5 + is sum of a and b
+
