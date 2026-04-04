86bdbad6dfc381344380de0c1dc454bf9090286e
Arunabh BH <arunabh2023@gmail.com>
2026-04-04 05:34:03 +0530

    Concatenation

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 //Sum of two integers
 
-let a = 5;
-let b = 10;
-let sum = a + b;
-console.log(sum);
+// let a = 5;
+// let b = 10;
+// let sum = a + b;
+// console.log(sum);
+
+//Concatenation
+
+let a = "Hello, ";
+let b = 2;
+console.log(typeof (b + a));
+
+// Here the the sum of a string and number is a string
