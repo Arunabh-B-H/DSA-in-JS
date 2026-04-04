3e99698f4f7e0dbd04deda94c5a377d4a10d4743
Arunabh BH <arunabh2023@gmail.com>
2026-04-04 07:06:56 +0530

    Accept and print

--- app.js
+++ app.js
@@ -13,15 +13,33 @@
 
 // Here the the sum of a string and number is a string
 
-let a = 2;
-let b = 3;
-console.log("Sum of a and b" + a + b);
-//Here the operand sum of a and b is in first priority so it takes the string and make the rest of the integers as string
-// Sum of a and b- which is a string +a =  Sum of a and b 2 + 5 = Sum of a and b 23
-//How to fix it?
-console.log("Sum of a and b is " + (a + b));
-//here the inside the bracket it gets the priority
-//if
-console.log(a + b  +  " is sum of a and b");
-//here the 2 + 5 + is sum of a and b
+// let a = 2;
+// let b = 3;
+// console.log("Sum of a and b" + a + b);
+// //Here the operand sum of a and b is in first priority so it takes the string and make the rest of the integers as string
+// // Sum of a and b- which is a string +a =  Sum of a and b 2 + 5 = Sum of a and b 23
+// //How to fix it?
+// console.log("Sum of a and b is " + (a + b));
+// //here the inside the bracket it gets the priority
+// //if
+// console.log(a + b  +  " is sum of a and b");
+// //here the 2 + 5 + is sum of a and b
 
+//Type Coercion
+// console.log("1" - 1);
+//here the js takes and converts the type of "1" to an integer and subtracts with the integer
+
+//same with * /
+
+//Accept and print
+
+let age = prompt("Enter your age ");
+
+console.log(age);
+
+//but here the age is a string
+//if we wanna get the input as number then we can use a Number() function
+
+age = Number(age);
+console.log(typeof age);
+//now the type of the input is number
