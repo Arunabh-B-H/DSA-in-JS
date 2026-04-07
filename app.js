372af1833759e2cd01d291747a640dc003c29789
Arunabh BH <arunabh2023@gmail.com>
2026-04-07 07:49:24 +0530

    done with swapping of two variables using three methods

--- app.js
+++ app.js
@@ -33,13 +33,40 @@
 
 //Accept and print
 
-let age = prompt("Enter your age ");
+// let age = prompt("Enter your age ");
 
-console.log(age);
+// console.log(age);
 
-//but here the age is a string
-//if we wanna get the input as number then we can use a Number() function
+// //but here the age is a string
+// //if we wanna get the input as number then we can use a Number() function
 
-age = Number(age);
-console.log(typeof age);
-//now the type of the input is number
+// age = Number(age);
+// console.log(typeof age);
+// //now the type of the input is number
+
+// Swapping two variables
+// let a = 10;
+// let b = 20;
+
+// Swapping with an extra variable
+
+// let c = a; // value of a is stored in c, c =10
+// a = b; // value of b is stored in a, a=20
+// b = c; // value of c is stored in b, b= 10
+// console.log(a, b);
+
+// Swapping without an extra variable
+
+// a = a + b; // a= 30
+// b = a - b; // b= 30-20 = 10
+// a = a - b; // a= 30-10 = 20
+// console.log(a, b);
+
+
+// Swapping with destructuring assignment
+
+// [a, b] = [b, a];
+// console.log(a, b);
+
+// Arithmetic operators
+
